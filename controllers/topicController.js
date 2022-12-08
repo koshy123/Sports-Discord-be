24d168cdb4913225fc38827f9129449370c1edbc
maxbond <ceo@rf.ru>
2022-12-08 21:36:34 +0300

    routes back

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,12 +1,28 @@
+const express = require('express');
+
+const Topic = require('../models/topic');
+
+const router = express.Router();
 
 
+router.get('/', (req, res) => {
+    Topic.find().then((topics) => res.json(topics));
+  });
+
+
+  router.post('/', async (req, res, next) => {
+    try {
+          const topics = await Topic.create(req.body)      
+      res.json(topics);
+    } catch (err) {
+      next(err);
+    }
+  });
 
   router.delete('/:id', (req, res) => {
     Topic.findOneAndDelete({
       _id: req.params.id,
-    }).then((deleteTopic) => res.json(deleteTopic));
+    }).then((deleteTopic) => res.json(deleteTopic)); //comments
   });
 
-
-
-  
+  module.exports = router;

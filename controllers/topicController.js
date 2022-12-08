45cd191e8de7aab76c29aeeb97ac026563d6f13c
humza15 <humzazafarullah@gmail.com>
2022-12-08 10:34:17 -0500

    topiccontroller

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -4,5 +4,19 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    Topic.find().then((burgers) => res.json(burgers));
+    Topic.find().then((topics) => res.json(topics));
   });
+//this is update
+router.put('/:id', (req, res) => {
+console.log(req.params.id)
+Topic.findOneAndUpdate({ _id: req.params.id }, req.body, {
+  new: true,
+}).then((adjustTopic) => res.json(adjustTopic));
+});
+//this is delete
+router.delete('/:id', (req, res) => {
+  Topic.findOneAndDelete({
+    _id: req.params.id,
+  }).then((deleteTopic) => res.json(deleteTopic));
+});
+module.exports = router;

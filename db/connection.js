fba6b75445b1fa56a0e5ce34dc50ccd8c5e0e1d1
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-08 10:18:29 -0500

    fix connection

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-
+const mongoURI =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost/sports-discord';
 
 mongoose
-  .connect('mongodb://localhost/sports-discord')
-  .then((instance) =>
+.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
   .catch((error) => console.log('Connection failed!', error));

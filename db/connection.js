1e49ee69d1d8a6aab20cc98121797a947b1c6805
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-08 09:38:54 -0500

    change the .connect to localhoat url

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/job-board')
+  .connect('mongodb://localhost/sports-discord')
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )

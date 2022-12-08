055de5391168d3e2788801c4d4954d7c7af56901
maxbond <ceo@rf.ru>
2022-12-08 19:26:24 +0300

    post

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -6,3 +6,15 @@ const router = express.Router();
 router.get('/', (req, res) => {
     Topic.find().then((topics) => res.json(topics));
   });
+
+  router.post('/', async (req, res, next) => {
+    try {
+          const topics = await Topic.create(req.body)
+      //const bookmarks = await Bookmark.find({}).populate('owner')
+      
+      res.json(topics);
+    } catch (err) {
+      
+      next(err);
+    }
+  });

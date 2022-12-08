ed0508c1d83a1bf47a4ba7b433fe9da991d15460
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-08 09:55:26 -0500

    fix connection file

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/sports-discord')
+    .connect('mongodb://localhost/job-board', {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )

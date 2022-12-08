c73dd5101312000820f072488a5afa00c26e9965
maxbond <ceo@rf.ru>
2022-12-08 20:31:31 +0300

    delete

--- controllers/topicController.js
+++ controllers/topicController.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const Topic = require('../models/topic');
+
 const router = express.Router();
 
 
@@ -7,14 +9,20 @@ router.get('/', (req, res) => {
     Topic.find().then((topics) => res.json(topics));
   });
 
+
   router.post('/', async (req, res, next) => {
     try {
-          const topics = await Topic.create(req.body)
-      //const bookmarks = await Bookmark.find({}).populate('owner')
-      
+          const topics = await Topic.create(req.body)      
       res.json(topics);
     } catch (err) {
-      
       next(err);
     }
   });
+
+  router.delete('/:id', (req, res) => {
+    Topic.findOneAndDelete({
+      _id: req.params.id,
+    }).then((deleteTopic) => res.json(deleteTopic));
+  });
+
+  module.exports = router;
